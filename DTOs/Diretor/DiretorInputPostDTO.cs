932994325730f2c3c63820a87347109911695722
Maike Moreira Mendes <67586377+maikemmendes@users.noreply.github.com>
2021-07-12 20:37:54 -0300

    Add FluentValidation in DiretorInput
    Adicionado FluentValidation ao InputPOST DTO

--- DTOs/Diretor/DiretorInputPostDTO.cs
+++ DTOs/Diretor/DiretorInputPostDTO.cs
@@ -1,7 +1,18 @@
 
+using FluentValidation;
+
 public class DiretorInputPostDTO
 {
     public string Nome { get; set;}
 
+}
+
+public class DiretorInputPostDTOValidator : AbstractValidator<DiretorInputPostDTO>{
+
+    public DiretorInputPostDTOValidator(){
+        RuleFor(diretor => diretor.Nome).NotNull().NotEmpty();
+        RuleFor(diretor => diretor.Nome).Length(2, 150).WithMessage("Tamanho de {TotalLength} é inválido");
+        
+    }
 }
     
